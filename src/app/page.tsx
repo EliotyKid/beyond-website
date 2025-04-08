dd592a0eb3af378e1262bdb36efb69418300d3a7
Eliort Kid <eliotfonse99@gmail.com>
2025-04-08 18:54:54 -0300

    feat(Elioty): Preloader Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client"
+import { useEffect, useState } from "react";
+import Preloader from "../components/Preloader/index"
+import { AnimatePresence } from "framer-motion";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+      document.body.style.cursor = 'default'
+    }, 2000)
+  })
+
   return (
     <main>
+      <AnimatePresence mode="wait">
+        { isLoading && <Preloader /> }
+      </AnimatePresence>
       <div>Hello world!</div>
     </main>
   );
